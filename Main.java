6e7cf134b2ef2cede6b9f37c8e5195944680b661
samuelm235 <108957962+samuelm235@users.noreply.github.com>
2024-12-11 14:30:39 +0000

    did stuff

---
+++ Main.java
@@ -0,0 +1,9 @@
+public class Main
+{
+    public static void main(String[] args) 
+    {
+        Sign sign1 = new Sign("ABC222DE", 3);
+        String str = sign1.getLines();
+        System.out.println(str);
+    }
+}
---
+++ Sign.java
@@ -0,0 +1,29 @@
+public class Sign
+{
+    private String message;
+    private int wide;
+    public Sign(String text, int width)
+    {
+        message = text;
+        wide = width;
+
+    }
+    public int numberOfLines()
+    {
+        if(message.length()%wide == 0) return message.length()/wide;
+        return message.length()/wide + 1;
+    }
+    public String getLines()
+    {
+        String divided = ""; 
+        for(int i = 0; i < message.length(); i += wide)
+        {
+            if(i + wide > message.length())
+            {
+                divided += message.substring(i);
+            }
+            divided += message.substring(i,i+wide) + ";";
+        }
+        return divided;
+    }
+}
